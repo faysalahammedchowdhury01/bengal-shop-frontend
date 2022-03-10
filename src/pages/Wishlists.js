af57d2c71b83f05f084bae58072d4cff26d51005
Faysal <74490913+faysalchowdhury99@users.noreply.github.com>
2022-03-10 17:49:53 +0600

    added something

--- src/pages/Wishlists.js
+++ src/pages/Wishlists.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import SectionHeading from '../components/SectionHeading';
 import WishlistsProducts from '../components/WishlistsProducts';
+import WishlistContext from '../contexts/wishlist/WishlistContext';
 import classes from '../styles/Wishlists.module.css';
 
-const Wishlists = () => (
-    <section className={classes.wishlistsSection}>
-        <div className="container">
-            <SectionHeading>
-                <h2 className="heading">Wishlists</h2>
-            </SectionHeading>
-            <WishlistsProducts />
-        </div>
-    </section>
-);
+const Wishlists = () => {
+    const { wishlists } = useContext(WishlistContext);
+
+    return (
+        <section className={classes.wishlistsSection}>
+            <div className="container">
+                <SectionHeading>
+                    <h2 className="heading">Wishlists</h2>
+                </SectionHeading>
+                {wishlists.length === 0 ? (
+                    <h3 className={classes.emptyMessage}>Wishlist is empty!</h3>
+                ) : (
+                    <WishlistsProducts />
+                )}
+            </div>
+        </section>
+    );
+};
 
 export default Wishlists;
