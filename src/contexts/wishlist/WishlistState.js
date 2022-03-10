b4e26b64e8dc663d665e0859b3530a50c2d2e9eb
Faysal <74490913+faysalchowdhury99@users.noreply.github.com>
2022-03-10 22:11:53 +0600

    change wishlist initial state

--- src/contexts/wishlist/WishlistState.js
+++ src/contexts/wishlist/WishlistState.js
@@ -5,38 +5,7 @@ import WishlistReducer from './WishlistReducer';
 
 const WishlistState = ({ children }) => {
     const initialState = {
-        wishlists: [
-            {
-                productId: '1',
-                img: 'https://raw.githubusercontent.com/faysalahammedchowdhury01/bengal-shop-frontend/main/src/assets/images/pomegranate.png',
-                name: 'Product Name - 1',
-                price: 750,
-                haveDiscount: true,
-                priceAfterDiscount: 550,
-                rating: 5,
-                ratingGiven: 220,
-            },
-            {
-                productId: '2',
-                img: 'https://raw.githubusercontent.com/faysalahammedchowdhury01/bengal-shop-frontend/main/src/assets/images/pomegranate.png',
-                name: 'Product Name - 2',
-                price: 700,
-                haveDiscount: true,
-                priceAfterDiscount: 600,
-                rating: 4.5,
-                ratingGiven: 150,
-            },
-            {
-                productId: '3',
-                img: 'https://raw.githubusercontent.com/faysalahammedchowdhury01/bengal-shop-frontend/main/src/assets/images/pomegranate.png',
-                name: 'Product Name - 3',
-                price: 450,
-                haveDiscount: true,
-                priceAfterDiscount: 400,
-                rating: 5,
-                ratingGiven: 262,
-            },
-        ],
+        wishlists: [],
     };
 
     const [state, dispatch] = useReducer(WishlistReducer, initialState);
